07abcb861ce27b5a1aa497c67d1e9a7c17af8bf1
乂Nat <83612604+Natx-C@users.noreply.github.com>
2022-07-23 03:36:30 +0700

    Add files via upload

--- system/config.js
+++ system/config.js
@@ -1,7 +1,7 @@
 // Owner number
-global.owner = '6285221100126'
+global.owner = '6285777451314'
 // Owner name
-global.owner_name = 'Wildan Izzudin'
+global.owner_name = 'nat'
 // Maximum upload file size limit (Default : 250 MB)
 global.max_upload = 250
 // Delay for spamming protection (Default : 3 seconds)
@@ -15,14 +15,14 @@ global.evaluate_chars = ['=>', '~>', '<', '>', '$']
 // Country code that will be automatically blocked by the system, when sending messages in private chat
 global.blocks = ['91', '92', '212']
 // Put target jid to forward friends story
-global.forwards = '6285221100126@c.us'
+global.forwards = '6285777451314@c.us'
 // Get neoxr apikey by registering at https://api.nxr.my.id
 global.Api = new (require('./neoxrApi'))('2lleJx')
 // Get bid and key configuration for autoreply chat ai feature by registering at https://brainshop.ai
 global.chatai_bid = '164728'
 global.chatai_key = 'MKPsfkgXLZPGrWoH'
 // Footer text
-global.footer = 'ꜱɪᴍᴘʟᴇ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴍᴀᴅᴇ ʙʏ ɴᴇᴏxʀ ッ'
+global.footer = 'simple whatsapp bot made by nat ッ\n@neoxr'
 // Global status
 global.status = Object.freeze({
    wait: Func.texted('bold', 'Processed . . .'),
